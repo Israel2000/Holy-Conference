67f7d1a7f1291813f16c1e055a776abea6216f16
israel belete <ibelete2000@gmail.com>
2022-07-22 10:34:47 -0600

    2nd commit of conf.site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SvgComponent } from './svg/svg.component';
 import { VTimelineComponent } from './v-timeline/v-timeline.component';
+import { AboutContactComponent } from './about-contact/about-contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SvgComponent,
-    VTimelineComponent
+    VTimelineComponent,
+    AboutContactComponent
   ],
   imports: [
     BrowserModule
